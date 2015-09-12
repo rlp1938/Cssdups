8a8ea8c5b06c2150b2e611fae03d8de235cae34c
bob <rlp1938@gmail.com>
2015-09-12 22:06:16 +0700

    Updated copyright on cssdups.c

--- cssdups.c
+++ cssdups.c
@@ -1,7 +1,7 @@
 
 /*      cssdups.c
  *
- *	Copyright 2011 Bob Parker <rlp1938@gmail.com>
+ *	Copyright 2015 Bob Parker <rlp1938@gmail.com>
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
